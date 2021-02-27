2e0caa9856c43fce4d7a5676b82ad1c3ea32f468
Chad Austin <chad@chadaustin.me>
2021-02-27 15:03:32 -0800

    [sculpt]
    Introduce a latency mode that lights an LED upon each keydown. This
    allows measurement, with a high-speed camera, of the delta between
    QMK's detection of the keypress event and the USB stack and host
    device.
    
    I've measured about 5 ms between the keyboard's LED lighting and the
    LED on a Raspberry Pi Zero with a simple program that reads events
    directly from /dev/input/event0 and lights the Pi's onboard LED. Same
    numbers on a Raspberry Pi 3B.

--- keyboards/handwired/sculpt/sculpt.c
+++ keyboards/handwired/sculpt/sculpt.c
@@ -8,4 +8,30 @@ void keyboard_pre_init_kb(void) {
     // The default fuse bit from the factory enables CLKDIV,
     // so enable full frequency in software.
     clock_prescale_set(clock_div_1);
+
+#if LATENCY_MODE_ENABLE
+    setPinOutput(LATENCY_MODE_PIN);
+#endif
 }
+
+
+#if LATENCY_MODE_ENABLE
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    static int count = 0;
+    if (record->event.pressed) {
+        ++count;
+#ifdef LATENCY_MODE_PIN
+        writePinHigh(LATENCY_MODE_PIN);
+#endif
+    } else {
+        --count;
+        if (count < 0) {
+            count = 0;
+        }
+        if (count == 0) {
+            writePinLow(LATENCY_MODE_PIN);
+        }
+    }
+    return process_record_user(keycode, record);
+}
+#endif
